9e0b7b98667a47a7b72b49a2f84d2b7ec9ccfe70
Daniel Hindrikes <daniel@hindrikes.se>
2025-01-15 18:43:46 +0100

    initial commit

---
+++ MauiCodeQL/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCodeQL
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiCodeQL/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiCodeQL
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
